4ced3e0967a6bc667e24e5a03da9d9fbaa9f87e2
pabsan-0 <psantana@catec.aero>
2023-10-09 07:27:54 +0200

    set obsidian to ws8 and xrandr script

--- i3/.config/i3/layouts/xrandr-single-monitor.sh
+++ i3/.config/i3/layouts/xrandr-single-monitor.sh
@@ -14,6 +14,10 @@ if xrandr | grep " connected" | wc -l; then
     
     elif xrandr | grep -q "DP-1-2 connected"; then
         xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off --output eDP-1-1 --off --output DP-1-1 --off --output DP-1-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2-1 --off --output DP-1-2-2 --off --output DP-1-2-3  --off
+
+    elif xrandr | grep -q "DP-1-1-3 connected"; then
+        xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off --output eDP-1-1 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-2-1 --off --output DP-1-2-2 --off --output DP-1-2-3 --off --output DP-1-1-3  --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+        
     else
         autorandr
         arandr
