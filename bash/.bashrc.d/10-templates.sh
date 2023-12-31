c1555b7b676cd449c653f73f91b82a57c94a190f
pabsan-0 <pablosantana@outlook.es>
2023-12-31 01:00:01 +0100

    improved templating

--- bash/.bashrc.d/10-templates.sh
+++ bash/.bashrc.d/10-templates.sh
@@ -1,23 +1,40 @@
 __templates_dir="$HOME/Templates"
 
-a () {
-    cp "$__templates_dir$1" "${2-.}"
+ftemplate () {
+    selected=$(cd $__templates_dir && fzf --preview 'cat {}') 
+    if [[ $selected ]]; then
+        cp -vi "$__templates_dir/$selected" "$PWD"
+    fi
 }
 
-__a_completion ()
+
+template () {
+    cp -vi "$__templates_dir/$1" "${2-.}"
+}
+
+__template_completion ()
 {
-    local cmd=$1 cur=$2 # pre=$3
+    local cmd=$1 cur=$2 pre=$3
     local arr i file
 
-    arr=( $( cd "$__templates_dir" && compgen -f -- "$cur" ) )
-    COMPREPLY=()
-    for ((i = 0; i < ${#arr[@]}; ++i)); do
-        file=${arr[i]}
-        if [[ -d $MEMO_DIR/$file ]]; then
-            file=$file/
-        fi
-        COMPREPLY[i]=$file
-    done
+
+    if [ $COMP_CWORD -gt 2 ]; then
+        COMPREPLY=()
+
+    elif [ $COMP_CWORD -gt 1 ]; then
+        compopt -o default
+
+    else
+        arr=( $( cd "$__templates_dir" && compgen -f -- "$cur" ) )
+        COMPREPLY=()
+        for ((i = 0; i < ${#arr[@]}; ++i)); do
+            file=${arr[i]}
+            if [[ -d $__templates_dir/$file ]]; then
+                file=$file/
+            fi
+            COMPREPLY[i]=$file
+        done
+    fi
 }
 
-complete -F __a_completion a
+complete -o nospace -F __template_completion template
