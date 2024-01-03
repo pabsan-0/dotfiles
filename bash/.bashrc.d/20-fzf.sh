d79d3ee580e044f4b5274528c213ffb3acc1ee0f
pabsan-0 <pablosantana@outlook.es>
2024-01-03 16:27:40 +0100

    vim.fzf like utils

--- bash/.bashrc.d/20-fzf.sh
+++ bash/.bashrc.d/20-fzf.sh
@@ -6,3 +6,31 @@ export FZF_DEFAULT_COMMAND="find \! \( -path '*/.git' -prune \) -printf '%P\n'"
 
 # Enable CtrlR and friends
 source /usr/share/doc/fzf/examples/key-bindings.bash
+
+
+# Vim.fzf like fzf, may receive program to run file as arg
+F () {
+    ${1-"echo"} $(
+        fzf --preview 'batcat --color always {} 2>/dev/null || tree {}' |
+        awk '{print $1;}'                                               |
+        xargs realpath                                                  ;
+    )
+}
+
+# Vim.fzf like ripgrep, may receive program to run file as arg
+R () {
+    ${1-"batcat"} $(
+        rg -m 1 -L --line-number --with-filename . --color=always --field-match-separator ' ' 2>/dev/null |
+        fzf --ansi --preview "batcat --color=always {1} --highlight-line {2}"                             | 
+        awk '{print $1;}'                                                                                 |
+        xargs realpath 2>/dev/null                                                                        ;
+    )
+}
+
+
+# Vim.fzf like Snippets util, may receive program to run file as arg
+S () {
+    pushd ~/snippets > /dev/null
+    R "${1-"batcat"}"
+    popd > /dev/null
+}
